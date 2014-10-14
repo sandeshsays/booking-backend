44bdb14d690a075f23abaec6a4c8e22a452f4322
maddou <laheen@gmail.com>
2014-10-14 22:48:12 +1100

    added base project structure

---
+++ booking.api/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace booking.api.Controllers
+{
+    [Route]
+    public class HomeController : ApiController
+    {
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            var message = Request.CreateResponse(HttpStatusCode.OK, ":)");
+
+            return message;
+        }
+    }
+}
---
+++ booking.dal/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("booking.dal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("booking.dal")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f3b6ac9e-ccd2-4714-89b3-30bc08aa317c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
